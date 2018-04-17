4596058e199a38119aa29d4c10c10b3c90cddab9
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-04-16 19:52:08 -0500

    pseudo coded game

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//Start game,
+    //type a letter for a guess,
+        //if its right,
+            //you win!
+            //increment wins,
+            //game restarts,
+                //clears guesses made array,
+                //changes guesses left to 10,
+        //if its wrong,
+            //deincrement the guesses left
+            //push wrong letter to guesses made array,
+                //if guesses left hits greater then 0,
+                    //guess again,
+                    //else if guess left equals 0,
+                        //you lose,
+                        //incremant losses.
+                        //restart game
+                            //clears guesses made array,
+                            //changes guesses left to 10,
