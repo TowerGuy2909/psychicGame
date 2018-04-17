333c95ff906ca205fa43b47db070deb7d72ef9a8
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-04-17 11:05:12 -0500

    added vars for wins, losses, guesses left and made. console log changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,8 @@
 //Start game,
+    //wins equal 0,
+    //losses equal 0,
+    //guesses left equal 10,
+    //guesses made is an empty array,
     //1. random letter is choosen from array,
         //2. random letter is hidden,
     //3. type a letter for a guess,
@@ -20,6 +24,10 @@
                             //19. clears guesses made array,
                             //20. changes guesses left to 10,
 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessesMade = []; 
 //1. random letter is choosen from an array.                           
 var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var randomLetter = letter[Math.floor(Math.random()* letter.length)];
@@ -36,9 +44,14 @@ function myKeyPress(e){
      //4. if its right,
     if(randomLetter === String.fromCharCode(keynum)){
          //5. you win!
-        alert("your pressed the random letter");
+        alert("You Win!");
+        wins = wins + 1;
+        console.log("Your wins: " + wins);
+
     } else {
         //10. if its wrong,
         alert("wrong letter");
+        guessesLeft = guessesLeft - 1;
+        console.log("Your guesses left: " + guessesLeft);
     }
 }
