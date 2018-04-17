d913330a7298e9868681d1566419be58966350fc
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-04-17 12:28:01 -0500

    altered pseudo, reset game in console on lose or win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 // Start game,
-    // wins equal 0,
-    // losses equal 0,
+    // total wins equal 0,
+    // total losses equal 0,
     // guesses left equal 10,
-    // guesses made is an empty array,
+    // guesses made starts as an empty array and where wrong guesses are pushed,
     // random letter is choosen from array,
         // random letter is hidden,
     // type a letter for a guess,
@@ -24,19 +24,24 @@
                         // you lose,
                         // incremant losses.
                         // restart game
+                            // picks new random letter,
                             // clears guesses made array,
                             // changes guesses left to 10,
 
+// total wins,
 var wins = 0;
+// total losses,
 var losses = 0;
+// guesses left,
 var guessesLeft = 10;
+// guesses made starts as an empty array and where wrong guesses are pushed,
 var guessesMade = []; 
-//1. random letter is choosen from an array.                           
+// random letter is choosen from an array.                           
 var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var randomLetter = letter[Math.floor(Math.random()* letter.length)];
-//2. not printing it out keeps it hidden.
+// not printing it out keeps it hidden.
 console.log(randomLetter);
-//3. type a letter for a guess,
+// type a letter for a guess,
 function myKeyPress(e){
     var keynum;
     if(window.event) { // IE                    
@@ -44,28 +49,48 @@ function myKeyPress(e){
     } else if (e.which){ // Netscape/Firefox/Opera                   
       keynum = e.which;
     }
-    //4. if its right,
+    // if its not in the array "Not a letter, guess again"
     if (letter.includes(String.fromCharCode(keynum)) === false){
         console.log("Not a letter, guess again");
+    // if you've already guessed the letter "already guessed, try again"   
     } else if (guessesMade.includes(String.fromCharCode(keynum)) === true) {    
         console.log("already guessed, try again")
+    // if its right,    
     } else if (randomLetter === String.fromCharCode(keynum)){
-            //5. you win!
-            alert("You Win!");
-            console.log("You Win!")
-            wins = wins + 1;
-            console.log("Your wins: " + wins);
-  
+        // you win!
+        alert("You Win!");
+        console.log("You Win!")
+        // increment wins,
+        wins = wins + 1;
+        console.log("Your wins: " + wins);
+        // game restarts, picks new random letter,
+        randomLetter = letter[Math.floor(Math.random()* letter.length)];
+        console.log(randomLetter);
+        // clears guesses made array,
+        guessesMade = [];
+        // changes guesses left to 10,
+        guessesLeft = 10;
+    } else {
+        // if its wrong,
+        alert("wrong letter");
+        guessesLeft = guessesLeft - 1;
+        console.log("Your guesses left: " + guessesLeft);
+        // else if guess left equals 0,
+        if (guessesLeft === 0){
+            // you lose,
+            console.log("You've no guesses left. You lose");
+            losses = losses + 1;
+            // game restarts, picks new random letter,
+            randomLetter = letter[Math.floor(Math.random()* letter.length)];
+            console.log(randomLetter);
+            // clears guesses made array,
+            guessesMade = [];
+            // changes guesses left to 10,
+            guessesLeft = 10;
         } else {
-            //10. if its wrong,
-            alert("wrong letter");
-            guessesLeft = guessesLeft - 1;
-            console.log("Your guesses left: " + guessesLeft);
-            if (guessesLeft === 0){
-                console.log("You've no guesses left. You lose");
-            } else {
-                guessesMade.push(String.fromCharCode(keynum));
-                console.log(guessesMade);
+            // push wrong letter to guesses made array,
+            guessesMade.push(String.fromCharCode(keynum));
+            console.log(guessesMade);
         }
     }
 }
